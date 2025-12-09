257356478cc6582eda77cfc997f0fcfb3667f83e
douzixx <1535906050@qq.com>
2025-12-09 19:19:50 +0800

    Clean up CWDset class by removing docstring
    Removed unnecessary docstring from CWDset class.

--- CWDset_code/CWD_dataset.py
+++ CWDset_code/CWD_dataset.py
@@ -3,13 +3,10 @@ from mmseg.datasets import BaseSegDataset
 
 @DATASETS.register_module()
 class CWDset(BaseSegDataset):
-    """
-    你自定义的 CWDset 数据集 "蓝图"。
-    """
-
+  
     METAINFO = dict(
         classes=('background', 'target'),
-        palette=[[0, 0, 0], [255, 0, 0]]  # 调色板
+        palette=[[0, 0, 0], [255, 0, 0]] 
     )
 
     def __init__(self,
